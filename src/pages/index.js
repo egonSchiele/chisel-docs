d2cb598d3683d81a429127f91843906250c4da84
Aditya Bhargava <adit@adit.io>
2023-04-25 11:26:45 -0500

    fix demo link

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,11 +15,14 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <a className="button button--primary button--lg" to="/login.html">
+          <a
+            className="button button--primary button--lg bg-black mr-4"
+            to="https://chiseleditor.com/register.html"
+          >
             Try Demo
           </a>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--secondary button--lg bg-gray-200"
             to="/docs/intro"
           >
             Read docs
