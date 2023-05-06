5fcaece0d3335432fcf14dfbf41923a5c74a07d3
Aditya Bhargava <adit@adit.io>
2023-05-06 13:19:51 -0500

    more features

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -34,29 +34,46 @@ export default function HomepageFeatures() {
       text: "Built-in AI prompts like expand, contract, and rewrite... and it's easy to add your own.",
     },
     {
-      image: useBaseUrl("/img/launcher.png"),
-      title: "Quick-launch",
-      text: "Navigate the UI, go to a chapter, or run an AI prompt, all without touching the mouse.",
+      image: useBaseUrl("/img/ask-question.png"),
+      title: "Talk to your text",
+      text: "Ask questions and get answers from your text.",
     },
     {
+      image: useBaseUrl("/img/folds.png"),
+      title: "Folds",
+      text: "Got a lot of text? Hide the parts you are not working on.",
+    },
+
+    /*   {
       image: useBaseUrl("/img/gridmode.png"),
       title: "Grid mode",
       text: "Organize your chapters by act or by beat using grid mode.",
-    },
+    }, */
     {
       image: useBaseUrl("/img/diff.png"),
       title: "Diff mode",
       text: "Compare two versions of your writing side-by-side.",
     },
-    /*     {
+
+    {
+      image: useBaseUrl("/img/launcher.png"),
+      title: "Quick-launch",
+      text: "Navigate the UI, go to a chapter, or run an AI prompt, all without touching the mouse.",
+    },
+    {
+      image: useBaseUrl("/img/done.png"),
+      title: "Chapter statuses",
+      text: "Easily see what's in progress and what's done.",
+    },
+    {
+      image: useBaseUrl("/img/character-sheet.png"),
+      title: "Character sheets",
+      text: "Keep track of your characters and their relationships.",
+    },
+    {
       image: useBaseUrl("/img/focusmode.png"),
       title: "Focus mode",
       text: "Get feedback on your writing.",
-    }, */
-    {
-      image: useBaseUrl("/img/folds.png"),
-      title: "Folds",
-      text: "Got a lot of text? Hide the parts you are not working on.",
     },
   ];
 
